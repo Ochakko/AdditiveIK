b8e1dcb92a2b58be2cbbf09ddb358b427564b953
Ochakko <info@ochakkolab.moo.jp>
2024-02-21 13:39:53 +0900

    1.0.0.9 RC1. Add Documents.

--- AdditiveIK/AdditiveIK.cpp
+++ AdditiveIK/AdditiveIK.cpp
@@ -14236,7 +14236,7 @@ LRESULT CALLBACK OpenMqoDlgProc(HWND hDlgWnd, UINT msg, WPARAM wp, LPARAM lp)
 				wfilename[0] = 0L;
 				WCHAR waFolderPath[MAX_PATH];
 				//SHGetSpecialFolderPath(NULL, waFolderPath, CSIDL_PROGRAMS, 0);//これではAppDataのパスになってしまう
-				swprintf_s(waFolderPath, MAX_PATH, L"C:\\Program Files\\OchakkoLAB\\AdditiveIK1.0.0.8\\Test\\");
+				swprintf_s(waFolderPath, MAX_PATH, L"C:\\Program Files\\OchakkoLAB\\AdditiveIK1.0.0.9\\Test\\");
 				ofn.lpstrInitialDir = waFolderPath;
 				ofn.lpstrFile = wfilename;
 
